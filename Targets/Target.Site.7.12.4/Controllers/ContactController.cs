fbd9c7935143c30bb008610df30973dea59a66cb
Steven Harland <stvnhrlnd@gmail.com>
2019-07-13 22:09:32 +0100

    Add 7.12.4 target

---
+++ Targets/Target.Site.7.12.4/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using Target.Site._7._12._4.Models;
+using Umbraco.Web.Mvc;
+
+namespace Target.Site._7._12._4.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult ContactForm()
+        {
+            var model = new ContactModel();
+            return PartialView("ContactForm", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HandleContact(ContactModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            TempData["HandleContactSuccess"] = true;
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ Targets/Target.Site.7.12.4/Models/ContactModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Target.Site._7._12._4.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
