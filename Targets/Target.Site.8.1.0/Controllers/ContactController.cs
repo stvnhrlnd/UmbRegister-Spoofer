a3ab3a05fc13e01b014f58eac4b6d4addcbdd54b
Steven Harland <stvnhrlnd@gmail.com>
2019-07-13 21:48:23 +0100

    Add Umbraco 8.1 target

---
+++ Targets/Target.Site.8.1.0/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using Target.Site._8._1._0.Models;
+using Umbraco.Web.Mvc;
+
+namespace Target.Site._8._1._0.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult ContactForm()
+        {
+            var model = new ContactModel();
+            return PartialView("ContactForm", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HandleContact(ContactModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            TempData["HandleContactSuccess"] = true;
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
