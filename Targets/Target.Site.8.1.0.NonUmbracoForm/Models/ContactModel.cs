0fa2d0aec1e5e97a3179bfafbf285cf103719949
Steven Harland <stvnhrlnd@gmail.com>
2019-07-13 22:24:15 +0100

    Add 8.1.0 target with non-Umbraco form

---
+++ Targets/Target.Site.8.1.0.NonUmbracoForm/Models/ContactModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Target.Site._8._1._0.NonUmbracoForm.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
