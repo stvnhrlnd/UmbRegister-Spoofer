39024d00c842775514ab8a7380b9987c0c380dd8
Steven Harland <stvnhrlnd@gmail.com>
2019-07-13 22:39:29 +0100

    Add 8.0.2 target

---
+++ Targets/Target.Site.8.0.2/Models/ContactModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Target.Site._8._0._2.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
